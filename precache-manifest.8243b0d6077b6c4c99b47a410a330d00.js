2f99392bdebdf6ffa221c8e1c0210573779259af
sreeharims98 <iamsreeharims@gmail.com>
2020-06-14 11:10:48 +0530

    Updates

---
+++ precache-manifest.8243b0d6077b6c4c99b47a410a330d00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb036127ee613c18f957831ba503b075",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "8992788e34830564596d",
+    "url": "/react-todo-app/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "ac2eb48df95ae4e513e0",
+    "url": "/react-todo-app/static/css/main.1fc6fb57.chunk.css"
+  },
+  {
+    "revision": "8992788e34830564596d",
+    "url": "/react-todo-app/static/js/2.5a67511f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-app/static/js/2.5a67511f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac2eb48df95ae4e513e0",
+    "url": "/react-todo-app/static/js/main.0e736aaa.chunk.js"
+  },
+  {
+    "revision": "f072e4f79f076f45002c",
+    "url": "/react-todo-app/static/js/runtime-main.a1924955.js"
+  }
+]);
